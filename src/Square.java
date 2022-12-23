59391d22d6082d5c5a05b917c9e30060a0bec527
ZiynatI <zisomurodova@gmail.com>
2022-12-23 22:35:13 +0500

    GameEngine updated

--- src/Square.java
+++ src/Square.java
@@ -2,8 +2,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Square {
-    public final int row;
     public final char column;
+    public final int row;
+
 
 
     public Square(char column, int row) {
@@ -20,7 +21,7 @@ public class Square {
         map.put(new Square('h', 8), new Pair[]{new Pair<>(new Square('g', 7), new Square('f', 6))});
         map.put(new Square('a', 7), new Pair[]{new Pair<>(new Square('b', 6), new Square('c', 5))});
         map.put(new Square('c', 7), new Pair[]{new Pair<>(new Square('b', 6), new Square('a', 5)), new Pair<>(new Square('d', 6), new Square('e', 5))});
-        map.put(new Square('e', 7), new Pair[]{new Pair<>(new Square('d', 6), new Square('c', 5), new Pair<>(new Square('f', 6), new Square('g', 5))});
+        map.put(new Square('e', 7), new Pair[]{new Pair<>(new Square('d', 6), new Square('c', 5)), new Pair<>(new Square('f', 6), new Square('g', 5))});
         map.put(new Square('g', 7), new Pair[]{new Pair<>(new Square('f', 6), new Square('e', 5))});
         return map;
     }
